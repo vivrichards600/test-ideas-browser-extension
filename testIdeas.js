9529b5018b4cb9da9ab720042fba7885497b2ce6
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2022-09-27 22:40:42 +0100

    Adding tabindex idea

--- testIdeas.js
+++ testIdeas.js
@@ -1,10 +1,11 @@
 {
 	const ideaMetaData = [
-		{condition: e => e.maxLength !== undefined && e.maxLength !== -1, idea: ["Field has a max length attribute set, investigate if removing this is useful"]},
-		{condition: e => e.required !== undefined && e.type !== 'hidden', idea: ["Field has a required attribute set, investigate if removing this is useful"]},
+		{condition: e => e.maxLength !== undefined && e.maxLength !== -1, idea: ["Field has a max length attribute set, investigate if removing this is useful."]},
+		{condition: e => e.required !== undefined && e.type !== 'hidden', idea: ["Field has a required attribute set, investigate if removing this is useful."]},
 		{condition: e => e.tagName === "INPUT" && e.type === "text", idea: ["Field is of type text and so will accept anything, explore the input range."]},
 		{condition: e => e.tagName === "INPUT" && e.type !== "text", idea: [`Field expects a particular input type, investigate if changing this is useful.`]},
-		{condition: e => e.type === "hidden", idea: ["Does this hidden field have a value that looks useful to amend?"]}
+		{condition: e => e.type === "hidden", idea: ["Does this hidden field have a value that looks useful to amend?"]},
+		{condition: e => e.getAttribute('tabindex') !== null && e.getAttribute('tabindex') >= 0, idea: ["Field has a tabindex set, investigate if this alters the expected tab order."]}
 	]
 
 	const elements = document.querySelectorAll('a[href], button, input, textarea, select');
